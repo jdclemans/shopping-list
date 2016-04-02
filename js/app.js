8084979339560f4c3a65c448df73d4774a7ab08a
Josh Clemans <j.d.clemans@gmail.com>
2016-04-02 08:20:53 -0700

    show/hide input working

---
+++ js/app.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+	$(".input").hide();
+
+  $('.add').mousedown(function() {
+  	$(".input").show();
+  });
+
+
+
+}
+);
+
+    // $(".lists").prepend("<li>Mousburgers</li>");
