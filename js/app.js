c1cd7752f5143f377679fdb8f734de240ddcb9d7
Josh Clemans <j.d.clemans@gmail.com>
2016-04-09 08:45:16 -0700

    style revamp

--- js/app.js
+++ js/app.js
@@ -1,35 +1,38 @@
 
 $(document).ready(function(){
-	$(".input").hide();
 
-  $('.add').mousedown(function() {
-  	$(".input").show();
+  $('.addLink').click(function(e) {
+  	$(".listForm").show();
+    e.preventDefault();
   }); //show input box
 
   $( ".add" ).click(function() {
-  $( "input" ).focus();
-}); //autofocus
+    $( "input" ).focus();
+  }); //autofocus
 
-  $( "input" )
-  .keyup(function(e) 
-  { if(e.which==13) {
-    var value = $( this ).val();
+  $('.listForm').submit(function(e) {
+    var value = $('.item').val();
     $( ".lists" ).prepend("<li>" + value + "</li>");
-  	$(".input").hide();
-  	$("input").val(" ")
-  } //End if
-  });
+    e.preventDefault();
+  })
 
- $('ul').on('click', 'li', function()
-  {
+  // $( "input" ).keyup(function(e) { 
+  //   if(e.which==13) {
+  //     var value = $( this ).val();
+  //     $( ".lists" ).prepend("<li>" + value + "</li>");
+  //   	$(".input").hide();
+  //   	$("input").val(" ")
+  //   } //End if
+  // });
+
+ $('ul').on('click', 'li', function() {
   	if ($(this).is("strike")) {
   		$(this).unwrap();
   	} 
   	else {
   		$(this).wrap("<strike></strike>");
   	}
-  console.log("working"); 	
-  }); //strike/unstrike
+  }); //strike or unstrike
 
   $('ul').on('dblclick', 'li', function() {
   	$(this).remove();
